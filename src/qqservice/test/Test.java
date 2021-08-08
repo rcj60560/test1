1086c76ad3e08b20e7e2cfe6ac8ac45a1f19b4aa
rcj60560 <289759379@qq.com>
2021-08-08 18:18:40 +0800

    本地第二次提交

--- src/qqservice/test/Test.java
+++ src/qqservice/test/Test.java
@@ -43,5 +43,6 @@ public class Test {
         System.out.println(car.getName());
         System.out.println("master 第一次提交 ");
         System.out.println("master 本地 第一次提交 ");
+        System.out.println("master 本地 第二次提交 ");
     }
 }
